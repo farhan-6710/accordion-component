a2fb75dbcec5b4a7cef3a5333b3fa37333db861d
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-03-07 03:51:49 +0530

    fix: disable multiple open items in accordion component and adjust transition duration

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <h2 className="accordion-heading" id="accordion-heading">
           Accordion Section
         </h2>
-        <AccordionComponent data={DUMMY_FAQ_DATA} multipleOpen={true} />
+        <AccordionComponent data={DUMMY_FAQ_DATA} multipleOpen={false} />
       </section>
     </main>
   );
